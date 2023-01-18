e5bd545cf164d6866c4877ef2fe5522a20d94e1c
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-01-18 14:43:06 +0000

    Updated CSS

--- BoatCo/tailwind.config.js
+++ BoatCo/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./resources/views/layouts/*.blade.php', './resources/views/*.blade.php'],
+  content: ['./resources/views/layouts/*.blade.php', './resources/views/*.blade.php','./public/js/*.js'],
   theme: {
     extend: {},
   },
